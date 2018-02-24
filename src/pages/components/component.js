e7ce66a7138afdd602087bba645bf984b4e6aba0
JohnGoo <1036611700@qq.com>
2018-02-24 17:37:46 +0800

    U 优化 同步本地代码

--- src/pages/components/component.js
+++ src/pages/components/component.js
@@ -1,3 +1,61 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
+/*
+class Form extends Component {
+	constructor(props, context) {
+		super(props);
+		this.state = {
+			
+		}
+	}
+	render() {
+		return (
+			<form onSubmit={this.props.bindsubmit}>
+				{this.props.children}
+			</form>
+		)
+	}
+}
+
+class Input extends Component {
+	constructor(props, context) {
+		super(props);
+		this.state = {
+			value: props.value
+		}
+	}
+	handleChange(e) {
+		this.setState({
+			value: e.target.value
+		})
+		console.dir(e.target.value);
+	}
+	render() {
+		return (
+			<input type={this.props.type} placeholder={this.props.placeholder} value={this.state.value} onChange={this.handleChange.bind(this)}></input>
+		)
+	}
+}
+
+class Button extends Component {
+	constructor(props, context) {
+		super(props);
+		this.state = {
+			
+		}
+	}
+	render() {
+		return (
+			<button type={this.props.type}>{this.props.children}</button>
+		)
+	}
+}
+
+Input.propTypes = {
+	text: PropTypes.string
+}
+export {
+	Form, Input, Button
+};
+*/
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link, hashHistory } from "react-router";
 
+// 后续了解：rc-form
+// import { Form, Input, Button } from "./components/component";
+
 import '../style/login.less';
 
 @connect((state, props) => ({}))
@@ -16,13 +19,28 @@ export default class Login extends Component {
   	componentDidMount() {
     	// this.props.dispatch(fetchLogin({ currentPage: 1 }))
   	}
+  	handleName(e) {
+		this.setState({
+			name: e.target.value
+		})
+  	}
+  	handlePassword(e) {
+		this.setState({
+			password: e.target.value
+		})
+  	}
   	handleSubmit(e) {
-		
+  		console.log('submit', this.state);
+		e.preventDefault();
   	}
 	render() {
 		return (
 			<div>
-				我是一个登陆界面
+				<form onSubmit={this.handleSubmit.bind(this)}>
+					<input name="username" type="text" placeholder="请输入用户名" onChange={this.handleName.bind(this)} />
+					<input name="password" type="password" placeholder="请输入密码" onChange={this.handlePassword.bind(this)} />
+					<button type="submit">提交</button>
+				</form>
 			</div>
 		)
 	}
